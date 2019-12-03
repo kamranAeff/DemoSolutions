0b1764a6a17c32110e12dc30edfc7749e0330537
kamran.abdullayev <kamran.abdullayev@PROGRAMMER>
2019-12-03 12:11:51 +0400

    Temp Files Clear

---
+++ TempCleanerService/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ServiceProcess;
+
+namespace TempCleanerService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            Service1.OnDebug();//for test
+
+            return;
+
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ TempCleanerService/Service1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+
+namespace TempCleanerService
+{
+    public partial class Service1 : ServiceBase
+    {
+        const string logPath = "errors.log";
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        public static void OnDebug()
+        {
+            
+            var tempFolder = Path.GetTempPath();
+
+            foreach (var path in Directory.GetFiles(tempFolder))
+            {
+                try
+                {
+                    File.Delete(path);
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
+
+            foreach (var path in Directory.GetDirectories(tempFolder))
+            {
+                try
+                {
+                    Directory.Delete(path, true);
+                }
+                catch (IOException ex)
+                {
+
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+            }
+        }
+    }
+}
